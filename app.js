b754fb0854174e64db2ecda5a5a54db5a0f4a7db
bboalimoe <bboalimoe@gmail.com>
2015-07-11 18:04:11 +0800

    refactoring the db reviewing

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ var bodyParser = require('body-parser');
 var todos = require('./routes/todos');
 var cloud = require('./cloud');
 
-var app = express();
-
+var app = require("./mongo-express/app.js");
+//var app = express()
 // 设置 view 引擎
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+
 
 // 加载云代码方法
 app.use(cloud);
@@ -20,8 +18,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-// 可以将一类的路由单独保存在一个文件中
-app.use('/todos', todos);
 
 // 如果任何路由都没匹配到，则认为 404
 // 生成一个异常让后面的 err handler 捕获
